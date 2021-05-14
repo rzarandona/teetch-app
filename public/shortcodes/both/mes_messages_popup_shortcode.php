8beab18c275ce9ce3b718985e6e11c9b89443b50
rzarandona <rein.zarandona@gmail.com>
2021-05-14 17:42:09 +0800

    update messages popup

--- public/shortcodes/both/mes_messages_popup_shortcode.php
+++ public/shortcodes/both/mes_messages_popup_shortcode.php
@@ -6,8 +6,30 @@
         position: fixed;
         top:0;
         left: 0;
-        background:orange;
         z-index: 999;
+        background: rgba(0,0,0,.3);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-family: "Nunito";
+    }
+
+    #mes-messages-popup .messages-list{
+        padding: 20px;
+        background: white;
+        border-radius: 10px;
+        width: 800px;
+    }
+
+    #mes-messages-popup .message-item{
+        display: grid;
+        grid-template-columns: 1fr 3fr 2fr;
+        align-items: center;
+    }
+
+    #mes-messages-popup .message-item .message{
+        font-size: .8em;
+        justify-self: center;
     }
 
 
@@ -19,7 +41,7 @@
         <div class="message-item">
             
             <div class="teacher">Arlene McCoy</div>
-            <div class="message">A schedule for you has been set on May 5, 2021 9h00</div>
+            <div class="message">A schedule for you has been set</div>
             <div class="schedule">May 5, 2021 9h00 - 10h45</div>
 
         </div>
@@ -27,7 +49,7 @@
         <div class="message-item">
             
             <div class="teacher">Arlene McCoy</div>
-            <div class="message">A schedule for you has been set on May 5, 2021 9h00</div>
+            <div class="message">A schedule for you has been set</div>
             <div class="schedule">May 5, 2021 9h00 - 10h45</div>
 
         </div>
@@ -35,7 +57,7 @@
         <div class="message-item">
             
             <div class="teacher">Arlene McCoy</div>
-            <div class="message">A schedule for you has been set on May 5, 2021 9h00</div>
+            <div class="message">A schedule for you has been set</div>
             <div class="schedule">May 5, 2021 9h00 - 10h45</div>
 
         </div>
